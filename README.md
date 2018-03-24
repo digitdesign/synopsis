435da2343681aa8e7251991756ee25be14d81b31
Shaan Shivanandan <shaan.shivanandan@godigit.com>
2018-03-24 11:57:23 +0530

    Added repository

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# synopsis
-Python3 solution for URL shortening using Google’s URL Shortener API (https://goo.gl/).
+<div align="center"><img src="https://d2h44aw7l5xdvz.cloudfront.net/synopsis/synopsis-site-id.svg" height="64" alt="Synopsis">
+	<p align="center">Python3 solution for batch URL shortening using Google’s URL Shortener API.</p>
+	<p align="center"><a href="https://github.com/digitdesign/synopsis/releases/latest"><img src="https://img.shields.io/github/release/digitdesign/synopsis.svg" alt=""></a> <a href="https://github.com/digitdesign/synopsis/find/master"><img src="https://img.shields.io/github/repo-size/digitdesign/synopsis.svg" alt=""></a> <a href="https://github.com/digitdesign/synopsis/search?l=css"><img src="https://img.shields.io/github/languages/top/digitdesign/synopsis.svg" alt=""></a></p>
+</div>
+<br />
+<hr />
+
+### Installation
+- [Download latest release](https://github.com/digitdesign/digitxd/archive/master.zip) or;
+- Clone the repository via command line:
+```sh
+git clone https://github.com/digitdesign/digitxd.git
+```
+
+### Contributing
+Feel free to dive in! [Open an issue](https://github.com/digitdesign/digitxd/issues/new/) or submit a PR.
+
+### License
+© 2016-2018, Go Digit General Insurance Ltd. or its affiliates
+
+### Acknowledgement
+This project is created and maintained by [Digit Design](https://godigit.design/).
+
+### Support
+[Send support request →](mailto:shaan.shivanandan@godigit.com?Subject=Support%3A%20Digit%20XD)
---
+++ synopsis.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Description
+"""Python3 solution for batch URL shortening using Google’s URL Shortener API (https://goo.gl/)."""
+
+# Package information
+__author__ = 'Go Digit General Insurance Ltd.'
+__version__ = '1.1.5'
+__status__ = 'Production'
+__maintainer__ = 'Shaan Shivanandan'
+__email__ = 'shaan.shivanandan@godigit.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2018 Go Digit General Insurance Ltd.'
+
+# Library imports
+import os
+import sys
+import json
+import config
+import logging
+import requests
+import pandas as pd
+
+# Library partials
+from time import sleep
+from random import randint
+
+# Set current working directory
+abspath = os.path.abspath(__file__)
+cwd = os.path.dirname(abspath)
+os.chdir(cwd)
+
+# Constants
+SOURCE = pd.read_csv('records.csv', names=['URLs'], header=0)
+API_KEY = config.API_KEY
+API_URL = 'https://www.googleapis.com/urlshortener/v1/url?key=' + API_KEY
+
+# Array declaration
+URLS = SOURCE['URLs'].tolist()
+URLS_SHORT = []
+
+
+# Function definitions
+def url_shorten(target):
+    post_url = API_URL.format(API_KEY)
+    headers = {'content-type': 'application/json'}
+    payload = {'longUrl': target}
+    sleep(randint(1, 2))
+    r = requests.post(post_url, data=json.dumps(payload), headers=headers)
+    return r.json()['id']
+
+
+def url_shorten_batch():
+    for i in URLS:
+        try:
+            URLS_SHORT.append(url_shorten(target=i))
+        except Exception as e:
+            logging.exception(e)
+
+
+def csv_export():
+    df = pd.DataFrame({'Original URLs': URLS, 'Short URLs': URLS_SHORT})
+    df.to_csv('output.csv', index=False, encoding='utf-8')
+
+
+# Function calls
+url_shorten_batch()
+csv_export()
+
+# Program exit
+sys.exit()
