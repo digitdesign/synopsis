3bcd40d980cca69d477670293a99b7c0902aec7b
Shaan Shivanandan <shaan.shivanandan@godigit.com>
2018-03-30 03:51:39 +0530

    Chore: Cleaned up easy-installation bash script

--- tools/install.sh
+++ tools/install.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# Basic
 set -o errexit
 set -o nounset
 set -o pipefail
-trap 'echo; echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+trap 'echo; echo "Aborting due to error on line $LINENO. Exit code: $?" >&2' ERR
 
-# Installation
 clear
 printf "\\033[0;33mCloning Repository\\033[0m\\n"
 sleep 1
@@ -15,10 +13,10 @@ cd synopsis;
 clear
 printf "\\033[0;33mConfiguration\\033[0m\\n"
 sleep 1
-read -e -p "Enter API Key → " APIKEY
-sed -i -e "s/YOUR_API_KEY_HERE/$APIKEY/g" config.py
+read -e -p "Enter API Key → " API_KEY
+sed -i -e "s/YOUR_API_KEY_HERE/$API_KEY/g" config.py
 clear
-printf "\\033[0;33mCleaning up installation\\033[0m\\n"
+printf "\\033[0;33mCleaning up installation…\\033[0m\\n"
 sleep 1
 rm -rf 'config.py-e'
 rm -rf 'tools'
@@ -27,4 +25,3 @@ clear
 printf "\\033[0;32mSetup Successful\\033[0m\\n"
 sleep 1
 echo "You’re done! Simply replace the ‘records.csv’ file with your desired list of URLs and run __init__.py…"
-echo
