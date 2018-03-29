da5f1fa44cf8c9626c2eacb101b0fe7b4be42724
Shaan Shivanandan <shaan.shivanandan@godigit.com>
2018-03-30 03:24:30 +0530

    Fix: Refined script for easy-install

--- tools/install.sh
+++ tools/install.sh
@@ -15,7 +15,9 @@ clear
 printf "\\033[0;33mConfiguration\\033[0m\\n"
 echo -n "Your API Key → "
 read APIKEY
-sed "s/YOUR_API_KEY_HERE/$APIKEY/g" config.py
+sed -i -e "s/YOUR_API_KEY_HERE/$APIKEY/g" config.py
+printf "\\033[0;33mCleaning up installation\\033[0m\\n"
+rm -rf 'config.py-e'
 clear
 printf "\\033[0;33mSetup Successful\\033[0m\\n"
 echo "You're done! Simply replace the 'records.csv' file with your desired list of URLs and run __init__.py"
