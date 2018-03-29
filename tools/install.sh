a0a9739d50a46255d2f8f742a18842a8611bd0ec
Shaan Shivanandan <shaan.shivanandan@godigit.com>
2018-03-30 03:17:42 +0530

    Fix: Refined script for easy-install

--- tools/install.sh
+++ tools/install.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+# Basic
+set -o errexit
+set -o nounset
+set -o pipefail
+trap 'echo; echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+# Installation
+clear
+printf "\\033[0;33mCloning Repository\\033[0m\\n"
 git clone https://github.com/digitdesign/synopsis;
 cd synopsis;
+clear
+printf "\\033[0;33mConfiguration\\033[0m\\n"
 echo -n "Your API Key → "
 read APIKEY
-git commit -am "$APIKEY" -s
-sed -i -e "s/YOUR_API_KEY_HERE/$APIKEY/g" config.py
+sed -i "s/YOUR_API_KEY_HERE/$APIKEY/g" config.py
 clear
+printf "\\033[0;33mSetup Successful\\033[0m\\n"
 echo "You're done! Simply replace the 'records.csv' file with your desired list of URLs and run __init__.py"
